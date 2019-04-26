4482f3d46c864057bf4a31dfc1a32d4cf872ab51
Sidney Alves <50020838+SidneyJunior01234@users.noreply.github.com>
2019-04-26 20:09:17 -0300

    Create Etapa1.js

---
+++ Etapa1.js
@@ -0,0 +1,17 @@
+/* 
+    Equipe: 
+        Sidney Alves dos Santos Junior - Subturma C (Líder) 
+        Claudiano Leonardo da Silva - Subturma A 
+        Etapa 1
+*/
+
+function setup() 
+{
+  createCanvas(400, 400);
+}
+
+function draw() 
+{
+  background(0);
+  rect(100,100,50,50);
+}
